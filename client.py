5af4cd83f937228f7a0219ef812bbf70f5982727
fedorov-yu <80154971+fedorov-yu@users.noreply.github.com>
2021-05-25 17:43:40 +0300

    Update client.py
    added comment

--- client.py
+++ client.py
@@ -3,14 +3,14 @@ from src.TRUE.Gamestatus import GameStatus
 
 
 #t.open_file()
-file = input('File location: ')
+file = input('File location: ') # You can choose path to your <file>.csv
 errors = int(input('Count of errors: '))
 t = TrueOrFalse(file, errors)
 t.open_file()
 while t.game_status == GameStatus.IN_PROGRESS:
-    print(t.current_question())
+    print(t.current_question()) 
     answer = input('Your answer Yes or No: ')
-    t.let_answer(answer)
+    t.let_answer(answer) # trying your answer
     t.next_question()
     if t.game_status == GameStatus.WIN:
         print('congratulations you WON!')
@@ -18,7 +18,7 @@ while t.game_status == GameStatus.IN_PROGRESS:
         print('sorry you LOST')
 
 
-#../data/Questions.csv
+#../data/Questions.csv my path to file
 
 
 
